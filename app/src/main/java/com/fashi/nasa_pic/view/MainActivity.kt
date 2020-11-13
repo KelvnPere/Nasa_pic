2352e7a4d248f02c985c544d1ffa6ede137d5d64
Pheonix73 <codebear73@gmail.com>
2020-11-13 14:26:41 +0100

    Added view and viewmodel packagage

--- app/src/main/java/com/fashi/nasa_pic/view/MainActivity.kt
+++ app/src/main/java/com/fashi/nasa_pic/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.fashi.nasa_pic
+package com.fashi.nasa_pic.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.fashi.nasa_pic.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
